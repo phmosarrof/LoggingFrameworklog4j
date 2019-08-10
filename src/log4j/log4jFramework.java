558796cfe5a7a1e6fe4b573b9ca102cbc0850a34
Mosarrof Hossain <phmosharaf@gmail.com>
2019-08-10 16:04:49 -0400

    Updated

--- src/log4j/log4jFramework.java
+++ src/log4j/log4jFramework.java
@@ -5,14 +5,16 @@ import org.apache.logging.log4j.Logger;
 
 public class log4jFramework {
 
-	
 	private static Logger log = LogManager.getLogger(log4jFramework.class.getName());
+
 	public static void main(String[] args) {
-		log.info("I did it successfully");
+		log.info("I did log4jFramework successfully");
 		log.error("The error is noticed");
-		//log.trace("Trace all the log");
+		// log.trace("Trace all the log");
 		log.debug("Debug message");
-		
+		log.debug("Debug message");
+		log.fatal("The problem is serious and need to solve as early as possible");
+		log.warn("Need to be attention immediately and resolved the issue");
 
 	}
 
